434bacb9cb68462ce6d6642c959751d1d585cbe8
noamba95 <noamba@post.bgu.ac.il>
2016-04-02 15:20:37 +0300

    the logger

---
+++ WASP/WASP/Logger/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASP.Logger
+{
+    interface Logger 
+
+    {
+        void writeToFile(string msg);
+    }
+}
