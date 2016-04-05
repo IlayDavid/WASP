8306e0ded23f7a38851cf5e451b136e7f39d9a2f
Ariel Talmi <talmia@post.ac.il>
2016-04-05 17:56:54 +0300

    changed post to update the replies. added tests to server

--- WASP/WASP/DataClasses/Post.cs
+++ WASP/WASP/DataClasses/Post.cs
@@ -32,6 +32,7 @@ namespace WASP
 
         public Post(String content, Member author, DateTime now, Post inReplyTo)
         {
+            InReplyTo.AddReply(this);
             _title = inReplyTo._title;
             _content = content;
             _id = _idCounter;
