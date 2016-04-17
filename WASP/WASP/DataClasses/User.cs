004f91069f2da1cc8be35dcb11d09191e5ba38b4
amitay140 <amitay140@gmail.com>
2016-04-17 16:33:51 +0300

    another changes

--- WASP/WASP/DataClasses/User.cs
+++ WASP/WASP/DataClasses/User.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WASP.DataClasses
 {
-    public class User
-    { 
+     public class User
+    {
         public string Name { get; set; }
-
         public String UserName { get; set; }
-
         public String Email { get; set; }
-
         public String Password { get; set; }
-
         
     }
 }
