3a37dc263d96afef87efda35570b74eab53af49a
Ariel Talmi <talmia@post.ac.il>
2016-04-28 02:49:29 +0300

    started adding new tests

---
+++ WASP/AccTests/Bridges/General/ClientSide/Driver.cs
@@ -0,0 +1,13 @@
+﻿namespace AccTests.Bridges.General.ClientSide
+{
+    public class Driver
+    {
+        public static WASPBridge getBridge()
+        {
+            ProxyBridge bridge = new ProxyBridge();
+            // add when real bridge is ready
+            bridge.proj = new RealBridge();
+            return bridge;
+        }
+    }
+}
