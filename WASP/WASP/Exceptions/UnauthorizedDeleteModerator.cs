52be3a92b536e5281af1bf919999b6e599679bc9
edan habler <edan habler>
2016-04-29 19:00:20 +0300

    add another exception :D

---
+++ WASP/WASP/Exceptions/UnauthorizedDeleteModerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASP.Exceptions
+{
+    class UnauthorizedDeleteModerator : UnauthorizedException
+    {
+        public UnauthorizedDeleteModerator(int userID, int modID) 
+            : base(userID, String.Format("delete moderator : {0}", modID))
+        {
+        }
+    }
+}
