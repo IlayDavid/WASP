dacfbaed53e8ece221b8a56c76d73ee90b17efad
edan habler <edan habler>
2016-04-29 18:35:29 +0300

    added unauthorizedCreateForum

---
+++ WASP/WASP/Exceptions/UnauthorizedCreateForum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WASP.Exceptions
+{
+    class UnauthorizedCreateForum : UnauthorizedException
+    {
+        public UnauthorizedCreateForum(int userID, int forumID) 
+            : base(userID, String.Format("create forum : {0}", forumID))
+        {
+        }
+    }
+}
