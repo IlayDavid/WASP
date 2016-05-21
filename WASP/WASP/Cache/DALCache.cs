8cc06b20302775e4e2710d7d092bcc450de6f916
matansar <matansar@post.bgu.ac.il>
2016-05-22 02:06:43 +0300

    Cache_V2

--- WASP/WASP/Cache/DALCache.cs
+++ WASP/WASP/Cache/DALCache.cs
@@ -12,17 +12,19 @@ namespace WASP.Cache
     {
         private const string _Forum = "Forum";
         private const string _Subforum = "Subforum";
+        private const string _Post = "Post";
+
+        private const string _SuperUser = "SuperUser";
         private const string _User = "User";
         private const string _Moderator = "Moderator";
         private const string _Admin = "Admin";
-        private const string _Post = "Post";
+        
 
         private TimeSpan _postExpiration = new TimeSpan(0, 1, 0);
         public void setPostExpiration(int hours, int minutes, int secs)
         {
             _postExpiration = new TimeSpan(hours, minutes, secs);
         }
-
 
         private ObjectCache _cache;
         
@@ -34,10 +36,13 @@ namespace WASP.Cache
         }
 
 
-
+        public void AddSuperUser(SuperUser su)
+        {
+            CacheItem forum = new CacheItem(su.Id.ToString(), su, _SuperUser);
+            _cache.Add(forum, new CacheItemPolicy());
+        }
         public void AddForum(Forum f)
         {
-            new CacheItemPolicy().SlidingExpiration = new TimeSpan(0, 1, 0);
             CacheItem forum = new CacheItem(f.Id.ToString(), f, _Forum);
             _cache.Add(forum, new CacheItemPolicy());
         }
@@ -48,17 +53,17 @@ namespace WASP.Cache
         }
         public void AddUser(User user)
         {
-            CacheItem us = new CacheItem(user.Id.ToString(), user, _User);
+            CacheItem us = new CacheItem(user.Id.ToString(), user, _User + user.Forum.Id);
             _cache.Add(us, new CacheItemPolicy());
         }
         public void AddModerator(Moderator mod)
         {
-            CacheItem moderator = new CacheItem(mod.Id.ToString(), mod, _Moderator);
+            CacheItem moderator = new CacheItem(mod.Id.ToString(), mod, _Moderator + mod.SubForum.Id);
             _cache.Add(moderator, new CacheItemPolicy());
         }
         public void AddAdmin(Admin admin)
         {
-            CacheItem adm = new CacheItem(admin.Id.ToString(), admin, _Admin);
+            CacheItem adm = new CacheItem(admin.Id.ToString(), admin, _Admin + admin.Forum.Id);
             _cache.Add(adm, new CacheItemPolicy());
         }
         public void AddPost(Post post)
@@ -69,6 +74,10 @@ namespace WASP.Cache
             _cache.Add(pst, policy);
         }
 
+        public void RemoveSuperUser(int id)
+        {
+            _cache.Remove(id.ToString(), _SuperUser);
+        }
         public void RemoveForum(int id)
         {
             _cache.Remove(id.ToString(),_Forum);
@@ -77,20 +86,42 @@ namespace WASP.Cache
         {
             _cache.Remove(id.ToString(), _Subforum);
         }
-        public void RemoveUser(int id)
+        public void RemoveUser(int id, int forum)
         {
-            _cache.Remove(id.ToString(), _User);
+            _cache.Remove(id.ToString(), _User + forum);
         }
-        public void RemoveModerator(int id)
+        public void RemoveModerator(int id, int subforum)
         {
-            _cache.Remove(id.ToString(), _Moderator);
+            _cache.Remove(id.ToString(), _Moderator + subforum);
         }
-        public void RemoveAdmin(int id)
+        public void RemoveAdmin(int id, int forum)
         {
-            _cache.Remove(id.ToString(), _Admin);
+            _cache.Remove(id.ToString(), _Admin + forum);
         }
 
-
-
+        public Forum GetForum(int f)
+        {
+            return (Forum)_cache.Get(f.ToString(), _Forum);
+        }
+        public Subforum GetSubforum(int sf)
+        {
+            return (Subforum)_cache.Get(sf.ToString(), _Subforum);
+        }
+        public SuperUser GetSuperUser(int su)
+        {
+            return (SuperUser)_cache.Get(su.ToString(), _SuperUser);
+        }
+        public User GetUser(int user, int forum)
+        {
+            return (User)_cache.Get(user.ToString(), _User + forum);
+        }
+        public Moderator GetModerator(int mod, int subforum)
+        {
+            return (Moderator)_cache.Get(mod.ToString(), _Moderator + subforum);
+        }
+        public Admin GetAdmin(int admin, int forum)
+        {
+            return (Admin)_cache.Get(admin.ToString(), _Admin + forum);
+        }
     }
 }
