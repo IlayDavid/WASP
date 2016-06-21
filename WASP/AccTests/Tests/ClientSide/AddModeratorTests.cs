0fe5eda207d01d7e1664fe16116a130140af06ee
noamba95 <noamba@post.bgu.ac.il>
2016-06-21 16:03:44 +0300

    tests

--- WASP/AccTests/Tests/ClientSide/AddModeratorTests.cs
+++ WASP/AccTests/Tests/ClientSide/AddModeratorTests.cs
@@ -14,6 +14,7 @@ namespace AccTests.Tests
         private WASPClientBridge _proj;
         private SuperUser _supervisor;
         private String supass="moshe123";
+        private String suusername = "SuperUser";
         private Admin _admin;
         private String adminpass="david123";
         private Forum _forum;
@@ -22,38 +23,33 @@ namespace AccTests.Tests
         private String modpass="ilan123";
         private User _member1;
         private String mempass="mem123";
-        private static int setup = 0;
 
         [TestInitialize]
         public void setUp()
         {
             Driver.getBridge().Clean();
             _proj = ClientDriver.getBridge();
-            if (setup == 0)
-            {
-                setup = 1;
+            if (_proj.isInitialize() == 0)
                 _supervisor = ClientFunctions.InitialSystem(_proj); //password is 
+            else
+            {
+                _proj.getAllForums();
+                _proj.loginSU(suusername, supass);
             }
-                var forumAndAdmin = ClientFunctions.CreateSpecForum(_proj, _supervisor);
-                _forum = forumAndAdmin.Item1;
-                _admin = forumAndAdmin.Item2; //password is david123
-                _proj.login(_admin.user.userName, adminpass, _forum.id);
+            var forumAndAdmin = ClientFunctions.CreateSpecForum(_proj, _supervisor);
+            _forum = forumAndAdmin.Item1;
+            _admin = forumAndAdmin.Item2; //password is david123
+            _proj.login(_admin.user.userName, adminpass, _forum.id);
 
 
-                var subforumAndModerator = ClientFunctions.CreateSpecSubForum(_proj, _admin, _forum);
-                _subforum = subforumAndModerator.Item1;
-                _moderator = subforumAndModerator.Item2; //password is ilan123
-                _proj.login(_moderator.userName, modpass, _forum.id);
+            var subforumAndModerator = ClientFunctions.CreateSpecSubForum(_proj, _admin, _forum);
+            _subforum = subforumAndModerator.Item1;
+            _moderator = subforumAndModerator.Item2; //password is ilan123
+            _proj.login(_moderator.userName, modpass, _forum.id);
 
 
-                _member1 = _proj.subscribeToForum(7, "mem1", "mem", "mem1@post.bgu.ac.il", "mem123", _forum.id); //password is ilan123
-                _proj.login(_member1.userName, mempass, _forum.id);
-        }
-
-        [TestCleanup]
-        public void TearDown()
-        {
-            //Driver.getBridge().Clean();
+            _member1 = _proj.subscribeToForum(7, "mem1", "mem", "mem1@post.bgu.ac.il", "mem123", _forum.id); //password is ilan123
+            _proj.login(_member1.userName, mempass, _forum.id);
         }
 
 
